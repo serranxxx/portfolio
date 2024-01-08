e8c15c26344447cc9666dfb3fb74377c0a451956
Alberto Serrano García <albserranog@Albertos-MacBook-Air.local>
2024-01-08 10:43:24 -0600

    update

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { LayoutApp } from '../layout/LayoutApp'
 import { Resume } from '../pages/Resume'
 import { HomePage } from '../pages/HomePage'
 import { Projects } from '../pages/Projects'
@@ -8,7 +7,7 @@ import { Projects } from '../pages/Projects'
 export const AppRouter = () => {
   return (
     <Routes>
-        <Route path ="/portfolio*" element ={<LayoutApp />} />
+        <Route path ="/portfolio*" element ={<HomePage />} />
         <Route path ="/portfolio/home" element ={<HomePage />} />
         <Route path ="/portfolio/projects" element ={<Projects />} />
         <Route path ="/portfolio/resume" element ={<Resume />} />
